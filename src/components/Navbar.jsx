1e37ef3bdde046c7087297753376d93259552462
Rodrigo José <74773266+RodrigoJose-Dev@users.noreply.github.com>
2022-10-24 18:13:33 -0300

    Navbar finalizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState } from "react";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+import { ImSun } from "react-icons/im";
+import { BsFillMoonFill } from "react-icons/bs";
+
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const [navState, setNavState] = useState(false);
+
+    return (
+        <nav>
+            <div className="brand-container">
+                <div className="brand">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="toggle-container">
+                    <div className="toggle"></div>
+                    <div className="mode"></div>
+                </div>
+            </div>
+            <div className="links-container">
+                <ul className="links">
+                    <li>
+                        <a href="#">Features</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Iniciar</a>
+                    </li>
+                    <li>
+                        <a href="#">Cadastrar-se</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
