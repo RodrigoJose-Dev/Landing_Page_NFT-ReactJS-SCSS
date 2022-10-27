5064085dcbff0286b0f505b8920945031facf1cf
Rodrigo José <74773266+RodrigoJose-Dev@users.noreply.github.com>
2022-10-27 15:51:21 -0300

    Criando Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+
+import logo from "../assets/logo.png";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    const links = [
+        {
+            title: "Sobre",
+            data: ["Sobre", "Termos", "Privacidade"],
+        },
+        {
+            title: "NFT",
+            data: ["OpenSea", "Criador", "Aprenda Sobre"],
+        },
+        {
+            title: "Contato",
+            data: ["E-mail", "Suporte"],
+        },
+        {
+            title: "Redes Sociais",
+            data: ["Discord", "Twitter", "Instagram"],
+        },
+    ];
+
+    const socialLink = [
+        <BsFacebook />,
+        <BsTwitter />,
+        <BsIntagram />,
+        <FaTiktok />,
+    ];
+
+    return (
+        <footer>
+            <div className="upper">
+                <div className="brand-container">
+                    <div className="brand">
+                        <img src={logo} alt="logo" />
+                    </div>
+                </div>
+            </div>
+            <div className="lower"></div>
+        </footer>
+    );
 };
 
 export default Footer;
